d13c9160ef7983b63d77102935cb97f30cd43d0a
Thayallan Srinathan <thayallan.srinathan@gmail.com>
2019-10-27 14:01:30 -0400

    rand

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ navigator.mediaDevices.getUserMedia({
 
     // Configures the signaling server.
     let hub = signalhub('video-chat', [
-        ' http://127.0.0.1:8080'
+        'https://fbfb003c.ngrok.io'
     ]);
 
     // Gives the stream to the swarm, to give to all the peers.
